17ced47d01c94bd1a82c158dffc1092566321514
AdejohOS <elusivebrown@gmail.com>
2022-06-15 08:16:02 +0100

    first edit

--- app/webapp/settings.py
+++ app/webapp/settings.py
@@ -29,14 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-
-ALLOWED_HOSTS = []
-ALLOWED_HOSTS.extend(
-    filter(
-        None,
-        os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(','),
-    )
-)
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
