e6caf45d5c9c8f3f24ad93177f93f954518effa6
AdejohOS <elusivebrown@gmail.com>
2022-06-22 18:15:18 +0100

    finito

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(live.koinoniaglobal.org)
 rsa_key_size=4096
-data_path="./nginx/certbot"
+data_path="./data/certbot"
 email="elusivebrown@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
